705adc995bd9312e021d5621b250e5464be78849
Gabriel Meireles <contato.gabrielmeireles@gmail.com>
2024-04-01 10:36:42 -0300

    Ready to Deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_portfolio/views/main_dashboard.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBU57KzgFIldxY4ingouY4jKF97TIVZe74",
+          authDomain: "gabrielmeireles-portfolio.firebaseapp.com",
+          projectId: "gabrielmeireles-portfolio",
+          storageBucket: "gabrielmeireles-portfolio.appspot.com",
+          messagingSenderId: "927988727531",
+          appId: "1:927988727531:web:7abd5a1b422b1ac0f948ae"));
   runApp(const MyApp());
 }
 
